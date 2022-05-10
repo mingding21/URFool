946d8de9e60c073288013c561866d2015741a339
mingding21 <105219805+mingding21@users.noreply.github.com>
2022-05-10 20:16:12 +0900

    MyPBL
    GUI Programing is so difficult... I am so sad. i can't satisfied myself.

---
+++ PBL/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PBL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelN1 = new System.Windows.Forms.Label();
+            this.labelN2 = new System.Windows.Forms.Label();
+            this.labelPlus = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelN1
+            // 
+            this.labelN1.AutoSize = true;
+            this.labelN1.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelN1.Location = new System.Drawing.Point(311, 84);
+            this.labelN1.Name = "labelN1";
+            this.labelN1.Size = new System.Drawing.Size(162, 96);
+            this.labelN1.TabIndex = 0;
+            this.labelN1.Text = "N1";
+            // 
+            // labelN2
+            // 
+            this.labelN2.AutoSize = true;
+            this.labelN2.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelN2.Location = new System.Drawing.Point(601, 84);
+            this.labelN2.Name = "labelN2";
+            this.labelN2.Size = new System.Drawing.Size(162, 96);
+            this.labelN2.TabIndex = 1;
+            this.labelN2.Text = "N2";
+            // 
+            // labelPlus
+            // 
+            this.labelPlus.AutoSize = true;
+            this.labelPlus.Font = new System.Drawing.Font("굴림", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelPlus.Location = new System.Drawing.Point(479, 84);
+            this.labelPlus.Name = "labelPlus";
+            this.labelPlus.Size = new System.Drawing.Size(100, 96);
+            this.labelPlus.TabIndex = 2;
+            this.labelPlus.Text = "+";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listBox1.Font = new System.Drawing.Font("굴림", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 56;
+            this.listBox1.Location = new System.Drawing.Point(40, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 60);
+            this.listBox1.TabIndex = 3;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("굴림", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelResult.Location = new System.Drawing.Point(287, 292);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(212, 48);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "두구두구";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelPlus);
+            this.Controls.Add(this.labelN2);
+            this.Controls.Add(this.labelN1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDownResult);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelN1;
+        private System.Windows.Forms.Label labelN2;
+        private System.Windows.Forms.Label labelPlus;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ PBL/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL
+{
+    public partial class Form1 : Form
+    {
+        public int sec = 3;
+        Random rnd = new Random();
+        int result;
+        int num1;
+        int num2;
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add(sec);
+
+            num1 = rnd.Next(0, 5);
+            num2 = rnd.Next(0, 6);
+
+            labelN1.Text = num1.ToString();
+            labelN2.Text = num2.ToString();
+
+            result = num1 + num2 ;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(sec);
+            sec--;
+            listBox1.Items.Add(sec);
+
+            if (sec == 0)
+            {
+                timer1.Enabled = false;
+                labelResult.Text = "너 바보구나";
+            }
+          
+        }
+
+
+        private void KeyDownResult(object sender, KeyEventArgs e)
+        {
+
+            result = (int)e.KeyCode - 48;
+            
+
+            if (result==(num1+num2))
+            {
+                timer1.Enabled = false;
+                labelResult.Text = "너 천재구나";
+
+  
+            }
+            else
+            {
+                timer1.Enabled = false;
+                labelResult.Text = " 너 바보구나";
+            }
+
+            
+        }
+    }
+}
